8122dc7e6565c03de32e84c3ea2f170ac2fbb51d
ffasitela <federico.d.fasitella@accenture.com>
2022-09-19 17:45:12 -0300

    Added a basic WebView component

---
+++ core/src/main/java/ar/com/wolox/wolmo/compose/core/webview/WolmoWebView.kt
@@ -0,0 +1,47 @@
+package ar.com.wolox.wolmo.compose.core.webview
+
+import android.graphics.Bitmap
+import android.view.ViewGroup
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+
+@Composable
+fun WolmoWebView(url: String) {
+    var backEnabled by remember { mutableStateOf(false) }
+    var webView: WebView? = null
+    Box(modifier = Modifier.fillMaxSize()) {
+        AndroidView(
+            factory = { context ->
+                WebView(context).apply {
+                    layoutParams = ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.MATCH_PARENT,
+                        ViewGroup.LayoutParams.MATCH_PARENT
+                    )
+                    webViewClient = object : WebViewClient() {
+                        override fun onPageStarted(view: WebView, url: String?, favicon: Bitmap?) {
+                            backEnabled = view.canGoBack()
+                            view.settings.javaScriptEnabled = true
+                            view.settings.domStorageEnabled = true
+                        }
+                    }
+                    loadUrl(url)
+                    webView = this
+                }
+            }, update = {
+                webView = it
+            })
+        BackHandler(enabled = backEnabled) {
+            webView?.goBack()
+        }
+    }
+}
